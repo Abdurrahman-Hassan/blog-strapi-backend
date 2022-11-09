9607eed6e4638f097591bf77c9a147c2fd88d4f1
Abdurrahman-Hassan <hanzalajj@gmail.com>
2022-11-09 18:44:44 +0500

    first

---
+++ config/database.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'blog'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'blogs'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: false,
+    },
+    debug: false,
+  },
+});
